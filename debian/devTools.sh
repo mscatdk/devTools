1a4cc89eeb1773e342a121973487a4d8abb2ed21
jksdfjsdjfk <kdjskdjfk@ksfkdjkjfds.com>
2019-04-07 14:27:09 +0200

    Initial version

---
+++ debian/devTools.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./docker_install.sh
+
+./java_install.sh
+
+./kubectl_install.sh
---
+++ debian/java_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Install Java tools
+sudo apt-get update
+sudo apt-get install -y git openjdk-8-jdk maven
